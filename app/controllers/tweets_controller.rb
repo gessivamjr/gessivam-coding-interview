7eda0ff305b3336a69cb67355ad1b01061395169
Gessivam Junior <juniorgessivam@gmail.com>
2025-04-09 16:03:26 -0300

    feat: add scope by_user to Tweet and filter tweets by user at TweetsController#index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,10 @@
 class TweetsController < ApplicationController
   def index
+    if params[:user_id].present?
+      user_tweets = Tweet.by_user(params[:user_id])
+      return render json: user_tweets
+    end
+
     render json: Tweet.all
   end
 end
