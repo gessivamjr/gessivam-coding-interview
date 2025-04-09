6d847debad7b93977cc9f84a0edf1174d717bcce
Gessivam Junior <juniorgessivam@gmail.com>
2025-04-09 16:33:07 -0300

    feat: add pagination to TweetsController#index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,6 +5,9 @@ class TweetsController < ApplicationController
       return render json: user_tweets
     end
 
-    render json: Tweet.all
+    page = params[:page]&.to_i || 1
+    per_page = params[:per_page]&.to_i || 5
+
+    render json: Tweet.all.limit(per_page).offset((page - 1) * per_page)
   end
 end
