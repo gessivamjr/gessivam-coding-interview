cb68473b03bf6f745bc79dc1cdb1d2f4b169c32a
Gessivam Junior <juniorgessivam@gmail.com>
2025-04-09 16:26:30 -0300

    test: add test to check users filtering by username correctly

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -24,17 +24,24 @@ RSpec.describe "Users", type: :request do
 
       it 'returns only the users for the specified company' do
         get company_users_path(company_1)
-        
+
         expect(result.size).to eq(company_1.users.size)
         expect(result.map { |element| element['id'] } ).to eq(company_1.users.ids)
       end
     end
 
     context 'when fetching all users' do
-      include_context 'with multiple companies'
+      it 'returns all the users by given username' do
+        user1 = create(:user, username: 'Gessivam')
+        user2 = create(:user, username: 'Adileia')
+        user3 = create(:user, username: 'Guilherme')
 
-      it 'returns all the users' do
+        get users_path(username: 'G')
 
+        expect(result.size).to eq(2)
+        expect(result.first['username'].start_with?('G')).to eq(true)
+        expect(result.first['username'][0]).to eq('G')
+        expect(result.map { |user| user['username'] }.exclude?('Adileia')).to eq(true)
       end
     end
   end
